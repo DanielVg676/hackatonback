f9b5ad30ff83815e8aae59002bb999a56a710a1f
DanielVg676 <156235653+DanielVg676@users.noreply.github.com>
2024-04-03 22:08:17 -0600

    Add files via upload

--- src/controller/traffic.controller.js
+++ src/controller/traffic.controller.js
@@ -1,1 +1,52 @@
+const mongoose = require('mongoose');
+const TrafficSchema = require('../model/traffic.model');
+const bcrypt= require('bcrypt');
 
+
+// Función para convertir el DataFrame a JSON y cargar los datos
+async function syncdata() {
+  // Simula la lectura del DataFrame preprocesado de Pandas
+  // En la práctica, necesitarías convertir df_preprocessed a un formato que Node.js pueda entender directamente o usar un archivo intermediario (como JSON)
+  const dfPreprocessed = new pandas.DataFrame( /* tus datos preprocesados aquí */);
+  const dataJson = dfPreprocessed.toJSON();
+
+  // Inserta los datos en MongoDB
+  try {
+    await traffic.insertMany(dataJson);
+    console.log('Datos cargados con éxito.');
+  } catch (error) {
+    console.error('Error al cargar datos:', error);
+  }
+}
+
+const manageTrafficData = async (operation, data, id = null, updateData = null) => {
+  try {
+    switch(operation) {
+      case 'save':
+        const newTrafficData = new Traffic(data);
+        await newTrafficData.save();
+        console.log('Datos del tráfico guardados con éxito.');
+        break;
+      case 'upload':
+        await Traffic.insertMany(data);
+        console.log('Datos del tráfico subidos con éxito.');
+        break;
+      case 'update':
+        if (id && updateData) {
+          const updatedTrafficData = await Traffic.findByIdAndUpdate(id, updateData, { new: true });
+          if (updatedTrafficData) {
+            console.log('Datos del tráfico actualizados con éxito.');
+          } else {
+            console.log('No se encontró el documento para actualizar.');
+          }
+        } else {
+          throw new Error('Para actualizar se requiere un ID y datos de actualización.');
+        }
+        break;
+      default:
+        throw new Error('Operación no válida.');
+    }
+  } catch (error) {
+    console.error(`Error al ${operation} datos del tráfico:`, error);
+  }
+};
--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,1 +1,20 @@
+const User = require('../model/user.model.js'); // Importa el modelo de usuario correctamente
+const express = require('express');
+const route = express.Router(); // Crea un nuevo router
 
+
+const login = async (req, res) => {
+  try {
+    // Asume que authenticate ahora espera solo un modelo relevante según el tipo de login
+    const { token, role } = await authenticate(req.body.email, req.body.password, User);
+    res.json({ message: 'User logged in successfully', token, role });
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+};
+
+
+module.exports = {
+  login
+};
+ // Exporta el router
