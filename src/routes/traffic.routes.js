8a6ecdff9e379afbc05468d734836ce54628a448
DanielVg676 <156235653+DanielVg676@users.noreply.github.com>
2024-04-03 22:10:58 -0600

    Add files via upload

--- src/routes/traffic.routes.js
+++ src/routes/traffic.routes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {login} = require('../controller/traffic.controller');
+
+
+const {
+    loaf
+
+
+} = require('controller/traffic.controller');
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { login } = require('../controller/users.controller');
 
+// Manejar la solicitud POST para la ruta /login
+router.post('/login', login);   
+
+module.exports = router;
