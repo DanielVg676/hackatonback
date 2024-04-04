aa22bce9171b464d6e829e4c100e2a0f93893086
DanielVg676 <156235653+DanielVg676@users.noreply.github.com>
2024-04-03 22:13:41 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
 
+const userRoutes = require('./routes/users.routes');
+const trafficRoutes = require('./routes/traffic.routes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/traffic', trafficRoutes);
+/* app.use('/api/pictures', profilePictureRoutes); */
+
+
+module.exports = app;
